387a77265d7448ae6acd1723dcb8b923ed32755c
AYUSH KUMAR <125579641+ayushmarch94@users.noreply.github.com>
2023-12-16 19:55:05 +0530

    1 card for song created

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
-var song= new Audio("1.mp3");
+
+// get song
+var song= new Audio("Adity.mp3");
+
+// get the div element card
 var clickedOn= document.querySelector("#one")
-console.log("check");
+
+var flag=1;
+
+//click event on div
 clickedOn.addEventListener("click",()=>{
-    song.play();
+
+    
+    //Play song
+    if(flag==1){
+        song.play();
+        flag=0;
+    } 
+    else{
+        song.pause()
+        flag=1;
+    }
+    
 })
